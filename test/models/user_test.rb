290943b726b8f1d760c15ba5e44c64a3f5ddd5a7
Michael Watts <hello@michaelwatts.me>
2014-12-22 22:28:42 +0000

    test for mixed case email address added

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -57,6 +57,13 @@ class UserTest < ActiveSupport::TestCase
     assert_not duplicate_user.valid?
   end
 
+  test "email should be saved as lowercase" do
+    mixed_case_email = "Foo@ExamPLE.cOM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
   test "password should be minimum length" do
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
