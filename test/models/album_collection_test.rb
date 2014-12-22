f917506d75c4722470a758dde5f114734b86c229
Michael Watts <hello@michaelwatts.me>
2014-12-22 21:30:24 +0000

    Make a User model with secure password

--- test/models/album_collection_test.rb
+++ test/models/album_collection_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class AlbumCollectionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+  # def setup
+  #   @collection = AlbumCollection.new(name: "An Album Collection", albums: "12, 2, 88, 99, 9")
+  # end
+  #
+  # test "should be valid album collection" do
+  #   assert @collection.valid?
   # end
 end
