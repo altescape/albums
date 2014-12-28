083c1e4a23577c436bd67b162af54bc21ebbe9ff
Michael Watts <hello@michaelwatts.me>
2014-12-28 21:35:01 +0000

    updated users schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141228115109) do
+ActiveRecord::Schema.define(version: 20141228213405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,10 +36,10 @@ ActiveRecord::Schema.define(version: 20141228115109) do
     t.string   "name"
     t.string   "email"
     t.string   "password"
-    t.integer  "albumCollectionId"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.string   "albums"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
