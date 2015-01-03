cf59d20d9bc343f05f1197e96e681628d03931cd
Michael Watts <hello@michaelwatts.me>
2015-01-03 00:30:23 +0000

    added @album_collection to #show action

--- app/controllers/album_collections_controller.rb
+++ app/controllers/album_collections_controller.rb
@@ -10,6 +10,7 @@ class AlbumCollectionsController < ApplicationController
   # GET /album_collections/1
   # GET /album_collections/1.json
   def show
+    @album_collection = AlbumCollection.find_by(user_id: current_user.id)
   end
 
   # GET /album_collections/new
@@ -65,7 +66,7 @@ class AlbumCollectionsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_album_collection
       # @album_collection = AlbumCollection.find(params[:id])
-      @album_collection = AlbumCollection.find_by user_id: current_user.id
+      @album_collection = AlbumCollection.find_by(user_id: current_user.id)
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -78,8 +78,8 @@ class UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find(session[:user_id])
       # @user = User.find(params['id'])
+      @user = User.find(session[:user_id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
