f852695aa5312aa9d38c09cc3b1780a008eb29e6
Michael Watts <hello@michaelwatts.me>
2015-01-05 22:59:52 +0000

    album collection tests updated

--- app/models/album_collection.rb
+++ app/models/album_collection.rb
@@ -1,4 +1,9 @@
 class AlbumCollection < ActiveRecord::Base
   belongs_to :user
   has_many :albums, foreign_key: "albumCollection_id"
+
+  before_save { self.name = name.titlecase }
+  after_update { self.name = name.titlecase }
+  validates :name, presence: true
+  validates :user_id, presence: true
 end
--- test/models/album_collection_test.rb
+++ test/models/album_collection_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
 
 class AlbumCollectionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    album_collection_params = {
+        id: 1,
+        name: "Name of Album collection",
+        user_id: 1
+    }
+    @album_collection = AlbumCollection.new(album_collection_params)
+  end
+
+  test "album collection should be valid" do
+    assert @album_collection.valid?
+  end
+
+  test "name should not be empty" do
+    @album_collection.name = ""
+    assert_not @album_collection.valid?
+  end
+
+  test "user id should not be empty" do
+    @album_collection.user_id = nil
+    assert_not @album_collection.valid?
+  end
+
+  test "name should be capitalized" do
+    @album_collection = AlbumCollection.find(1)
+    @album_collection.update(name: "bazzers album collection")
+
+    expected = "Bazzers Album Collection"
+    assert_equal expected, @album_collection.name
+  end
 end
