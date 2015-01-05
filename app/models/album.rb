baf269ec27fa974eda74cfa130524ec9c58cdda7
Michael Watts <hello@michaelwatts.me>
2015-01-05 21:15:41 +0000

    added validation to album model

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,3 +1,8 @@
 class Album < ActiveRecord::Base
   belongs_to :album_collection
+
+  validates :name, presence: true
+  validates :image, presence: true
+  validates :band, presence: true
+  validates :position, presence: true
 end
