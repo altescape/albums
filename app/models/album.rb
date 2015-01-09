cd4f5526bc05a0fefb0d06ba3d6df7275e731684
altescape <hello@michaelwatts.me>
2015-01-09 17:00:26 +0000

    added outline of replace album with same position function

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,6 +2,8 @@ class Album < ActiveRecord::Base
 
   belongs_to :album_collection
 
+  before_create :check_for_existing
+
   validates :name, presence: true
   validates :image, presence: true
   validates :artist, presence: true
@@ -15,6 +17,12 @@ class Album < ActiveRecord::Base
         position: self.position
         # @release_date = Date.strptime(@album.releaseDate.to_s, '%Y-%m-%d').year
     }
+  end
+
+  def check_for_existing
+    # query all album collection and retrieve album ids
+    # query each album and check position doesnt equal current position
+    # if it does then remove previous album and replace with this new one
   end
 
 end
