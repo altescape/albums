f4840479d3402c32c4fb1a8feea26bb8496f4eb3
altescape <hello@michaelwatts.me>
2015-01-09 15:03:08 +0000

    Added create_name function and removed redundant callbacks

--- app/models/album_collection.rb
+++ app/models/album_collection.rb
@@ -3,9 +3,12 @@ class AlbumCollection < ActiveRecord::Base
   belongs_to :user
   has_many :albums, dependent: :destroy
 
-  before_save { self.name = name.titlecase }
-  after_update { self.name = name.titlecase }
+  before_validation { create_name }
 
   validates :name, presence: true
   validates :user_id, presence: true
+
+  def create_name
+    self.name = User.find(user_id).name + "'s Desert Island Discs"
+  end
 end
