59c9aa416b44e277fc426488ae7356005cd65487
altescape <hello@michaelwatts.me>
2015-01-09 15:10:28 +0000

    tidy routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,25 +2,13 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
 
-  get 'sessions/new'
-
-  get 'static_pages/home'
-  get 'static_pages/help'
-
-  resources :albums
-  get 'albums/new/:p/:k', to: 'albums#new'
-  delete 'your_top_5/:id', to: 'albums#destroy'
+  resources :users, :album_collections, :albums
 
   resources :search, only: :search
   get 'search', to: 'search#search'
   get 'search/:p', to: 'search#search'
 
-  resources :album_collections
-  put 'your_top_5/position/:p/album/:a/', to: 'albums#create'
-  get 'your_top_5/position/:p/album/:a/', to: 'album_collections#show'
-  get 'your_top_5', to: 'album_collections#show'
-
-  resources :users
+  # User
   get 'profile', to: 'users#show', as: :show
   get 'register', to: 'users#new'
 
@@ -29,5 +17,14 @@ Rails.application.routes.draw do
 
   get 'logout', to: 'sessions#destroy'
   delete 'logout', to: 'sessions#destroy'
+
+  # Album collection
+  put 'your_top_5/position/:p/album/:a/', to: 'albums#create'
+  get 'your_top_5/position/:p/album/:a/', to: 'album_collections#show'
+  get 'your_top_5', to: 'album_collections#show'
+
+  # Albums
+  get 'albums/new/:p/:k', to: 'albums#new'
+  delete 'your_top_5/:id', to: 'albums#destroy'
 
 end
