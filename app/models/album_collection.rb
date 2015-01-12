ab90c388fb373e33da5ac55a27b6c562dce9c70b
Michael Watts <hello@michaelwatts.me>
2015-01-12 20:38:42 +0000

    removed name for album collection

--- app/models/album_collection.rb
+++ app/models/album_collection.rb
@@ -3,12 +3,7 @@ class AlbumCollection < ActiveRecord::Base
   belongs_to :user
   has_many :albums, dependent: :destroy
 
-  before_validation { create_name }
-
   validates :name, presence: true
   validates :user_id, presence: true
 
-  def create_name
-    self.name = User.find(user_id).name + "'s Desert Island Discs"
-  end
 end
