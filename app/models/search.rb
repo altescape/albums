6cd9bfbf2a2ed445e22d6cc6b75414c41dd23a1a
altescape <hello@michaelwatts.me>
2015-01-13 14:31:53 +0000

    redundant function

--- app/models/search.rb
+++ app/models/search.rb
@@ -12,20 +12,7 @@ class Search
 
   def search_albums(keywords = nil)
     if keywords
-      self.set_placeholder("Searching #{keywords}")
       @api.search(keywords)
     end
-  end
-
-  def get_placeholder
-    self.placeholder
-  end
-
-  def set_placeholder(message)
-    self.placeholder = message
-  end
-
-  def search
-
   end
 end
