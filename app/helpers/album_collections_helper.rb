dbe4eb96f808940b554648f21ed35e937aebf559
Michael Watts <hello@michaelwatts.me>
2015-01-18 00:42:48 +0000

    added random background image

--- app/helpers/album_collections_helper.rb
+++ app/helpers/album_collections_helper.rb
@@ -1,2 +1,5 @@
 module AlbumCollectionsHelper
+  def randomized_image
+    ["background-green.jpg", "background-yellow.jpg", "background-woman.jpg", "background-illustration.jpg"].sample
+  end
 end
